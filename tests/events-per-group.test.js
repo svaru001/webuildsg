efa8c23500b84bedfaa1bb8eef87552eb556a2b0
Sanket <sv@smartkarma.com>
2020-03-26 18:38:37 +0800

    Streamlined testcases

--- tests/events-per-group.test.js
+++ tests/events-per-group.test.js
@@ -3,46 +3,50 @@ import assert from 'assert';
 const linkdata = require('../data/event-per-groups/data')
 const expect = require('chai').expect
 const fetch = require('node-fetch')
+import allureReporter from '@wdio/allure-reporter'
 
 describe('Test Events Per Group Page', function () {
+
     before(() => {
         EventsPerGroupPage.open()
     })
 
-
-    it('Should display Header: "we build!" ', function () {
+    afterEach(() => {
+        if (!(browser.getUrl() == 'https://webuildsg.github.io/data/dataset/events-per-group/'))
+            EventsPerGroupPage.open()
+    })
+//Header Test Cases
+    it('Should display Header', function () {
         assert.equal(EventsPerGroupPage.header.isDisplayed(), true)
     })
 
-    it('Should go to main page if clicked on :main icon', function () {
+    it('Should go to main page if clicked on main icon', function () {
         EventsPerGroupPage.headerLinkMain.click()
         assert.equal($('//h1[contains(.,\'404\')]').isDisplayed(), true)
-        EventsPerGroupPage.open()
     })
-    it('Should go to apps page if clicked on :apps icon', function () {
+
+    it('Should go to apps page if clicked on apps icon', function () {
         EventsPerGroupPage.headerLinkApps.click()
         assert.equal($('//h1[contains(.,\'404\')]').isDisplayed(), true)
-        EventsPerGroupPage.open()
     })
-    it('Should go to live webuild page if clicked on :live music icon', function () {
+
+    it('Should go to live webuild page if clicked on live music icon', function () {
         EventsPerGroupPage.headerLinkLiveMusic.click()
         assert.equal(browser.getUrl(), 'https://live.webuild.sg/')
-        EventsPerGroupPage.open()
     })
-    it('Should go to api webuild page if clicked on :api icon', function () {
+
+    it('Should go to api webuild page if clicked on api icon', function () {
         EventsPerGroupPage.headerLinkApi.click()
         assert.equal(browser.getUrl(), 'https://api.webuild.sg/')
-        EventsPerGroupPage.open()
     })
 
-
-
-
-    it('Should display :Subtitle ', function () {
+    it('Should display Subtitle ', function () {
         assert.equal(EventsPerGroupPage.subTitleText.isDisplayed(), true)
     })
 
-    it('Check if all groups listed have atleast 1 event scheduled', function () {
+//Page Content Test Cases
+
+it('Check if all groups listed have atleast 1 event scheduled', function () {
         let check = true
         EventsPerGroupPage.barValues.forEach(value => {
             if (isNaN(value.getText()))
@@ -50,7 +54,6 @@ describe('Test Events Per Group Page', function () {
         })
         assert.equal(check, true)
     })
-
 
     it('should open event links in new tab and come back to origin', function () {
         let selector = EventsPerGroupPage.getdynamicGroupSelector('.NET Developers Singapore')
@@ -66,7 +69,7 @@ describe('Test Events Per Group Page', function () {
     })
 
     linkdata.forEach(data => {
-        it('Check if \'' + data.text + '\' has link : \'' + data.link + '\'', function () {
+        it('Check if \'' + data.text + '\' has correct link', function () {
             let selector = EventsPerGroupPage.getdynamicGroupSelector(data.text)
             console.log('sss' + selector.getAttribute('href').toLowerCase())
             console.log('ttt' + data.link.toLowerCase())
@@ -86,10 +89,65 @@ describe('Test Events Per Group Page', function () {
         })
         console.log('sss' + selector.getAttribute('href').toLowerCase())
         console.log('ttt' + data.link.toLowerCase())
-        assert.equal(selector.getAttribute('href').toLowerCase(), data.link.toLowerCase())
+        assert.equal(selector.getAttribute('href').toLowerCase(), data.link.toLowerCase(),'Found Broken Link')
     })
 
+    // Footer Test Cases
+    it('Should show Data Source note ', function () {
+        assert.equal(EventsPerGroupPage.sourceNote.isDisplayed(), true)
+    })
+    
+    it('Should show Open Event Bottom Box ', function () {
+        assert.equal(EventsPerGroupPage.openEventsPlaceholder.isDisplayed(), true)
+    })
+    
+    it('Should show Open Source Repository Explaination ', function () {
+        assert.equal(EventsPerGroupPage.openSourceRepoPlaceholder.isDisplayed(), true)
+    })
+    
+    it('Should open Github Event Data page when click on link \'Open Events Data\'', function () {
+        EventsPerGroupPage.openEventsDataLink.click()
+        assert.equal(browser.getUrl().includes('/data/events'), true)
+    })
+    
+    it('Should open Github Repo Source page when click on link \'Open Source Data\'', function () {
+        EventsPerGroupPage.openSourceDataLink.click()
+        assert.equal(browser.getUrl().includes('/data/repos'), true)
+    })
+    
+    it('Should redirect to twitter handler when click on \'suggestion\' link ', function () {
+        browser.refresh()
+        EventsPerGroupPage.suggestionLink.click()
+        assert.equal(browser.getUrl().includes('https://twitter.com/webuildsg'), true)
+    })
 
-
-
+    it('Should redirect to GitHub Repo when clicked on  \'report bugs send pull requests\' link ', function () {
+        EventsPerGroupPage.socialGithub.click()
+        assert.equal(browser.getUrl().includes('https://github.com'), true)
+    })
+    
+    it('Should redirect to Twitter page when clicked on  \'Twitter social\' icon ', function () {
+        EventsPerGroupPage.socialTwitter.click()
+        assert.equal(browser.getUrl().includes('https://twitter.com'), true)
+    })
+    
+    it('Should redirect to Facebook page when clicked on  \'facebook social\' icon ', function () {
+        EventsPerGroupPage.socialFacebook.click()
+        assert.equal(browser.getUrl().includes('https://www.facebook.com/webuildsg'), true)
+    })
+    
+    it('Should redirect to GitHub page when clicked on  \'GitHub social\' icon ', function () {
+        EventsPerGroupPage.socialGithub.click()
+        assert.equal(browser.getUrl().includes('https://github.com/webuildsg'), true)
+    })
+    
+    it('Should redirect to WeBuildSg RSS feel when clicked on  \'RSS social\' icon ', function () {
+        EventsPerGroupPage.socialRss.click()
+        assert.equal(browser.getUrl().includes('https://feeds.feedburner.com/WeBuildLive'), true)
+    })
+    
+    it('Should redirect to Apple Podcast when clicked on  \'iTunes\' icon ', function () {
+        EventsPerGroupPage.socialItunes.click()
+        assert.equal(browser.getUrl().includes('https://podcasts.apple.com/us/podcast/we-build-sg-live/id713804010'), true)
+    })
 })
---
+++ tests/repos-per-prog-lang.test.js
@@ -0,0 +1,212 @@
+import ReposPerProgLangPage from '../pages/repos-per-prog-lang.page';
+import assert from 'assert';
+const expect = require('chai').expect
+import allureReporter from '@wdio/allure-reporter'
+import reposPerProgLangPage from '../pages/repos-per-prog-lang.page';
+
+describe('Events Per Group Page:', function () {
+  
+  before(() => {
+    ReposPerProgLangPage.open()
+  })
+  
+  afterEach(() => {
+    if (!(browser.getUrl() == 'https://webuildsg.github.io/data/dataset/repos-per-programming-language/'))
+      ReposPerProgLangPage.open()
+
+    if (browser.getUrl().includes('#'))
+      ReposPerProgLangPage.open()
+  })
+  
+  it('Should display Header', function () {
+    assert.equal(ReposPerProgLangPage.header.isDisplayed(), true)
+  })
+
+  it('Should go to main page if clicked on main icon', function () {
+    ReposPerProgLangPage.headerLinkMain.click()
+    assert.equal($('//h1[contains(.,\'404\')]').isDisplayed(), true)
+  })
+  
+  it('Should go to apps page if clicked on apps icon', function () {
+    ReposPerProgLangPage.headerLinkApps.click()
+    assert.equal($('//h1[contains(.,\'404\')]').isDisplayed(), true)
+  })
+  
+  it('Should go to live webuild page if clicked on live music icon', function () {
+    ReposPerProgLangPage.headerLinkLiveMusic.click()
+    assert.equal(browser.getUrl(), 'https://live.webuild.sg/')
+  })
+  
+  it('Should go to api webuild page if clicked on api icon', function () {
+    ReposPerProgLangPage.headerLinkApi.click()
+    assert.equal(browser.getUrl(), 'https://api.webuild.sg/')
+  })
+
+  it('Should display Subtitle ', function () {
+    assert.equal(ReposPerProgLangPage.subTitleText.isDisplayed(), true)
+  })
+
+  it('Should compute number of listed language ', function () {
+    assert.equal(ReposPerProgLangPage.allProgLangSelector.length, 20)
+  })
+
+  it('Should show repos when click on language C++', function () {
+    ReposPerProgLangPage.languageCPlus.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+  
+  it('Should show repos when click on language Clojure', function () {
+    ReposPerProgLangPage.languageClojure.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+  
+  it('Should show repos when click on language CoffeScript', function () {
+    ReposPerProgLangPage.languageCoffeescript.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+ 
+  it('Should show repos when click on language CSS', function () {
+    ReposPerProgLangPage.languageCss.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+ 
+  it('Should show repos when click on language Go', function () {
+    ReposPerProgLangPage.languageGo.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Haskell', function () {
+    ReposPerProgLangPage.languageHaskell.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language HTML', function () {
+    ReposPerProgLangPage.languageHtml.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+ 
+  it('Should show repos when click on language Java', function () {
+    ReposPerProgLangPage.languageJava.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language JavaScript', function () {
+    ReposPerProgLangPage.languageJavascript.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Julia', function () {
+    ReposPerProgLangPage.languageJulia.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Objective C', function () {
+    ReposPerProgLangPage.languageObjectiveC.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language PHP', function () {
+    ReposPerProgLangPage.languagePhp.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Python', function () {
+    ReposPerProgLangPage.languagePython.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language R', function () {
+    ReposPerProgLangPage.languageR.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Ruby', function () {
+    ReposPerProgLangPage.languageRuby.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Rust', function () {
+    ReposPerProgLangPage.languageRust.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Shell', function () {
+    ReposPerProgLangPage.languageShell.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Swift', function () {
+    ReposPerProgLangPage.languageSwift.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show repos when click on language Tex', function () {
+    ReposPerProgLangPage.languageTex.click()
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should open repository page when click on repo link', function () {
+    ReposPerProgLangPage.languageC.click()
+    ReposPerProgLangPage.repoArray.click()
+    assert.equal(browser.getUrl().includes('https://github.com/'), true)
+  })
+
+  it('Should show Data Source note ', function () {
+    assert.equal(ReposPerProgLangPage.sourceNote.isDisplayed(), true)
+  })
+
+  it('Should show Open Event Bottom Box ', function () {
+    assert.equal(ReposPerProgLangPage.openEventsPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should show Open Source Repository Explaination ', function () {
+    assert.equal(ReposPerProgLangPage.openSourceRepoPlaceholder.isDisplayed(), true)
+  })
+
+  it('Should open Github Event Data page when click on link \'Open Events Data\'', function () {
+    ReposPerProgLangPage.openEventsDataLink.click()
+    assert.equal(browser.getUrl().includes('/data/events'), true)
+  })
+
+  it('Should open Github Repo Source page when click on link \'Open Source Data\'', function () {
+    ReposPerProgLangPage.openSourceDataLink.click()
+    assert.equal(browser.getUrl().includes('/data/repos'), true)
+  })
+
+  it('Should redirect to twitter handler when click on \'suggestion\' link ', function () {
+    browser.refresh()
+    ReposPerProgLangPage.suggestionLink.click()
+    assert.equal(browser.getUrl().includes('https://twitter.com/webuildsg'), true)
+  })
+
+  it('Should redirect to GitHub Repo when clicked on  \'report bugs send pull requests\' link ', function () {
+    ReposPerProgLangPage.socialGithub.click()
+    assert.equal(browser.getUrl().includes('https://github.com'), true)
+  })
+
+  it('Should redirect to Twitter page when clicked on  \'Twitter social\' icon ', function () {
+    ReposPerProgLangPage.socialTwitter.click()
+    assert.equal(browser.getUrl().includes('https://twitter.com'), true)
+  })
+
+  it('Should redirect to Facebook page when clicked on  \'facebook social\' icon ', function () {
+    ReposPerProgLangPage.socialFacebook.click()
+    assert.equal(browser.getUrl().includes('https://www.facebook.com/webuildsg'), true)
+  })
+
+  it('Should redirect to GitHub page when clicked on  \'GitHub social\' icon ', function () {
+    ReposPerProgLangPage.socialGithub.click()
+    assert.equal(browser.getUrl().includes('https://github.com/webuildsg'), true)
+  })
+
+  it('Should redirect to WeBuildSg RSS feel when clicked on  \'RSS social\' icon ', function () {
+    ReposPerProgLangPage.socialRss.click()
+    assert.equal(browser.getUrl().includes('https://feeds.feedburner.com/WeBuildLive'), true)
+  })
+
+  it('Should redirect to Apple Podcast when clicked on  \'iTunes\' icon ', function () {
+    ReposPerProgLangPage.socialItunes.click()
+    assert.equal(browser.getUrl().includes('https://podcasts.apple.com/us/podcast/we-build-sg-live/id713804010'), true)
+  })
+
+})
